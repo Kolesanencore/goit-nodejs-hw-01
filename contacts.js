d012c0c541b9942058659a9f440aa489b0eea7bf
Kolesanencore <kolesanencore@gmail.com>
2023-07-08 12:25:59 +0300

    added try catch in contacts.js

--- contacts.js
+++ contacts.js
@@ -7,27 +7,51 @@ import { nanoid } from "nanoid";
 const contactsPath = path.resolve("db/contacts.json");
 
 export async function listContacts() {
-  const data = await fs.readFile(contactsPath, "utf-8");
-  return JSON.parse(data);
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    throw new Error(error.message);
+  }
 }
 
 export async function getContactById(contactId) {
-  const contacts = await listContacts();
-  const resultById = contacts.find((contact) => contact.id === contactId);
-  return resultById || null;
+  try {
+    const contacts = await listContacts();
+    const resultById = contacts.find((contact) => contact.id === contactId);
+    return resultById || null;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 }
 
 export async function removeContact(contactId) {
-  const contacts = await listContacts();
-  const contactToRemove = contacts.find((contact) => contact.id === contactId);
-  if (!contactToRemove) return null;
-
-  const updatedContacts = contacts.filter(
-    (contact) => contact.id !== contactId
-  );
-  await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2));
-  return contactToRemove;
+  try {
+    const contacts = await listContacts();
+    const contactToRemove = contacts.find(
+      (contact) => contact.id === contactId
+    );
+    if (!contactToRemove) return null;
+    const updatedContacts = contacts.filter(({ id }) => id !== contactId);
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2));
+    return contactToRemove;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 }
+
+export async function addContact(name, email, phone) {
+  try {
+    const contacts = await listContacts();
+    const newContact = { id: nanoid(), name, email, phone };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
 // Когда большие массивы лучше через find или findndex ??
 
 // export async function removeContact(contactId) {
@@ -44,11 +68,3 @@ export async function removeContact(contactId) {
 //   await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2));
 //   return contactToRemove;
 // }
-
-export async function addContact(name, email, phone) {
-  const contacts = await listContacts();
-  const newContact = { id: nanoid(), name, email, phone };
-  contacts.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return newContact;
-}
